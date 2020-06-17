495e1e74d3d230682bb7b0665db4b6e1e16aedaf
amineux <amine.atarboulaich@gmail.com>
2020-06-17 23:35:41 +0200

    Update pyfla.py

--- pyfla.py
+++ pyfla.py
@@ -42,13 +42,13 @@ def winning_move(board, piece):
 			if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
 				return True
 
-	# Vérifier les positions digonals +
+	# Vérifier les positions digonals plus
 	for c in range(COLUMN_COUNT-3):
 		for r in range(ROW_COUNT-3):
 			if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
 				return True
 
-	# Vérifier les positions digonals -
+	# Vérifier les positions digonals moins
 	for c in range(COLUMN_COUNT-3):
 		for r in range(3, ROW_COUNT):
 			if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
